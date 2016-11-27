7a6932984175658806c11e86257391d6c136ea27
Caleb Power <clpower95@gmail.com>
2016-11-27 17:06:33 -0600

    made CardTree runnable within ArithmeticMachine; removed herobrine

--- src/edu/uco/cs/cowtippingdwarfs/card24/arithmeticmachine/ArithmeticMachine.java
+++ src/edu/uco/cs/cowtippingdwarfs/card24/arithmeticmachine/ArithmeticMachine.java
@@ -1,13 +1,32 @@
 package edu.uco.cs.cowtippingdwarfs.card24.arithmeticmachine;
 
 public class ArithmeticMachine {
+
+  Thread cardTreeThread = null;
+  CardTree cardTree = null;
   
-  OpenParanthesis firstTree = null;
-  CardNode secondTree = null;
+  public void solve(Card[] cards) {
+    cardTree = new CardTree(cards);
+    cardTreeThread = new Thread(cardTree);
+  }
   
-  ArithmeticMachine(Card[] cards) {
-    firstTree = new OpenParanthesis(this);
-    secondTree = new CardNode(this, cards[0].getValue());
+  public class CardTree implements Runnable {
+    
+    Card[] cards;
+    
+    public CardTree(Card[] cards) {
+      this.cards = cards;
+    }
+    
+    OpenParanthesis firstTree = null;
+    CardNode secondTree = null;
+
+    @Override
+    public void run() {
+      firstTree = new OpenParanthesis(this);
+      secondTree = new CardNode(this, cards[0].getValue());
+    }
+    
   }
   
 }
