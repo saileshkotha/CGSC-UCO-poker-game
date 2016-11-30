92121e778e17dad8fd481f15db7ce9870ddbb0f3
Caleb Power <clpower95@gmail.com>
2016-11-30 00:41:23 -0600

    added combinational logic machine

--- src/edu/uco/cs/cowtippingdwarfs/card24/arithmeticmachine/ArithmeticMachine.java
+++ src/edu/uco/cs/cowtippingdwarfs/card24/arithmeticmachine/ArithmeticMachine.java
@@ -16,23 +16,50 @@ public class ArithmeticMachine {
   
   public class CardTree implements Runnable {
     
-    int[] cardValues;
+    int[][] cardValues = new int[24][4];
     
     public CardTree(Card[] cards) {
-      cardValues = new int[cards.length];
-      for(int i = 0; i < cards.length; i++) {
-        cardValues[i] = cards[i].getValue();
+
+      for(int i = 0; i < 24; i++) { //generate first column of indexes
+        cardValues[i][0] = i / 6;
       }
-    }
+      
+      for(int i = 0; i < 4; i++) { //generate second column of indexes
+        for(int j = 0, k = 0; j < 6; j += 2, k++) {
+          if(cardValues[6 * i + j][0] == k) k++;
+          cardValues[6 * i + j][1] = cardValues[6 * i + j + 1][1] = k;
+        }
+      }
+      
+      for(int i = 0; i < 12; i++) { //generate third column of indexes
+        for(int j = 0, k = 0; j < 2; j++, k++) {
+          if(cardValues[2 * i + j][0] == k) k++;
+          if(cardValues[2 * i + j][1] == k) {
+            if(cardValues[2 * i + j][0] == ++k) k++;
+          }
+          cardValues[2 * i + j][2] = k;
+        }
+      }
+      
+      for(int i = 0; i < 24; i++) { //generate fourth column of indexes
+        cardValues[i][3] = 6 - cardValues[i][2] - cardValues[i][1] - cardValues[i][0];
+      }
+      
+      for(int i = 0; i < 24; i++) { //replace indexes with values;
+        for(int j = 0; j < 4; j++) {
+          cardValues[i][j] = cards[cardValues[i][j]].getValue();
+        }
+      }
     
-    OpenParanthesis firstTree = null;
-    CardNode secondTree = null;
+    }
 
     @Override
     public void run() {
 
-      firstTree = new OpenParanthesis(cardValues, -1, "");
-      secondTree = new CardNode(cardValues, -1, "");
+      for(int i = 0; i < 24; i++) {
+        new OpenParanthesis(cardValues[i], -1, "");
+        new CardNode(cardValues[i], -1, "");
+      }
       System.out.println("Displaying results.");
       Core.displayResults();
       
